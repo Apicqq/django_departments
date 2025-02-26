febacc51b0424a49c831171f7f3e2e7018e9e155
Nikita Smykov <132088678+Apicqq@users.noreply.github.com>
2025-02-26 18:59:06 +0300

    feat: create admin panel's stubs

--- django_departments/departments/admin.py
+++ django_departments/departments/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
 
+from departments.models import Department, Employee
+
+
 # Register your models here.
+
+
+class EmployeeInline(admin.TabularInline):
+    model = Employee
+
+
+@admin.register(Department)
+class DepartmentAdmin(admin.ModelAdmin):
+    inlines = [EmployeeInline]
+
+
+@admin.register(Employee)
+class EmployeeAdmin(admin.ModelAdmin):
+    pass
