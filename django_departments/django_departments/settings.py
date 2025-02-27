9be7ad411f7a987932f1517005353a614af7dd92
Nikita Smykov <132088678+Apicqq@users.noreply.github.com>
2025-02-27 12:03:37 +0300

    feat: add django-bootstrap to remove external calls for js and css files

--- django_departments/django_departments/settings.py
+++ django_departments/django_departments/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "django_bootstrap5",
     "departments.apps.DepartmentsConfig",
     "core.apps.CoreConfig",
 ]
