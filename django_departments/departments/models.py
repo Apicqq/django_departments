d36a91809483b40e7429ef0e7def40f57e4a68be
Nikita Smykov <132088678+Apicqq@users.noreply.github.com>
2025-02-27 11:01:43 +0300

    feat: add validators to model's fields, recreate initial migration

--- django_departments/departments/models.py
+++ django_departments/departments/models.py
@@ -1,8 +1,17 @@
 from django.db import models
 
+from departments.validators import min_length_validator, validate_hided_at, \
+    salary_min_value_validator
+
 
 class Department(models.Model):
-    name = models.CharField("Название департамента", max_length=100)
+    name = models.CharField(
+        "Название департамента",
+        max_length=100,
+        validators=[
+            min_length_validator
+        ]
+    )
     parent = models.ForeignKey(
         "self",
         on_delete=models.CASCADE,
@@ -22,13 +31,27 @@ class Department(models.Model):
 
 
 class Employee(models.Model):
-    # TODO Добавить констрейнты на имя (не пустое), должность (не пустое),
-    #  зарплата (больше 0), дата приема (не будущая), не пустое подразделение,
-    #  не более одного подразделения у сотрудника
-    full_name = models.CharField("ФИО", max_length=100)
-    position = models.CharField("Должность", max_length=100)
-    date_hired_at = models.DateField("Дата приема на работу")
-    salary = models.DecimalField("Зарплата", max_digits=10, decimal_places=2)
+    full_name = models.CharField(
+        "ФИО", max_length=100,
+        validators=[min_length_validator]
+    )
+    position = models.CharField(
+        "Должность",
+        max_length=100,
+        validators=[
+            min_length_validator
+        ]
+    )
+    date_hired_at = models.DateField(
+        "Дата приема на работу",
+        validators=[validate_hided_at]
+    )
+    salary = models.DecimalField(
+        "Зарплата",
+        max_digits=10,
+        decimal_places=2,
+        validators=[salary_min_value_validator]
+    )
     department = models.ForeignKey(
         Department,
         on_delete=models.CASCADE,
@@ -40,10 +63,6 @@ class Employee(models.Model):
         ordering = ("full_name",)
         verbose_name = "Сотрудник"
         verbose_name_plural = "Сотрудники"
-        constraints = [
-            # ...
-            #TODO add later!
-        ]
 
     def __str__(self):
         return f"{self.pk}: {self.full_name} - {self.position}"
---
+++ django_departments/departments/validators.py
@@ -0,0 +1,17 @@
+import datetime
+from decimal import Decimal
+
+from django.core.exceptions import ValidationError
+from django.core.validators import MinLengthValidator, MinValueValidator
+from django.utils.timezone import now
+
+min_length_validator = MinLengthValidator(1)
+
+salary_min_value_validator = MinValueValidator(Decimal("0.01"))
+
+
+def validate_hided_at(value: datetime.date) -> None:
+    if value > now():
+        raise ValidationError(
+            "Дата приема на работу не может быть в будущем."
+        )
