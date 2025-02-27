51243c9e981ce4c8a7511a5a09782dc04b275a78
Nikita Smykov <132088678+Apicqq@users.noreply.github.com>
2025-02-27 11:59:18 +0300

    feat: create templates and views for department tree structure

--- django_departments/departments/views.py
+++ django_departments/departments/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.views.generic.list import ListView
 
-# Create your views here.
+from departments.models import Department
+
+
+class DepartmentListView(ListView):
+    model = Department
+    template_name = "departments/department_list.html"
+    paginate_by = 15
+    context_object_name = "departments"
+    queryset = Department.objects.filter(parent__isnull=True).prefetch_related(
+        "subdepartments", "employees"
+    )
