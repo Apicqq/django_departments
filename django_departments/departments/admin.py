bda15fc1b5b18e1d12b23ceba5a015c0ebad39b4
Nikita Smykov <132088678+Apicqq@users.noreply.github.com>
2025-02-27 11:01:56 +0300

    feat: add simple admin panels for models

--- django_departments/departments/admin.py
+++ django_departments/departments/admin.py
@@ -8,11 +8,18 @@ from departments.models import Department, Employee
 
 class EmployeeInline(admin.TabularInline):
     model = Employee
+    extra = 0
+    readonly_fields = ("full_name", "position", "date_hired_at", "salary")
 
 
 @admin.register(Department)
 class DepartmentAdmin(admin.ModelAdmin):
+    list_display = ("name", "parent", "employee_count")
     inlines = [EmployeeInline]
+
+    @admin.display(description="Количество сотрудников в подразделении")
+    def employee_count(self, obj):
+        return obj.employees.count()
 
 
 @admin.register(Employee)
